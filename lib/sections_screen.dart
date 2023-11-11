918fcceecf0ad60d2be77cbe98c8e9ae41556a98
Adam Velebil <pardalek@gmail.com>
2023-11-11 07:27:45 +0100

    refactor sections screen, add section card

--- lib/sections_screen.dart
+++ lib/sections_screen.dart
@@ -88,45 +88,9 @@ class _SectionsScreenChildState extends State<_SectionsScreenChild> {
                   itemCount: sections != null ? sections!.length : 0,
                   itemBuilder: (BuildContext context, int index) {
                     final section = sections!.elementAt(index);
-                    return ListTile(
-                      leading: const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.insert_drive_file_outlined),
-                        ],
-                      ),
-                      trailing: PopupMenuButton<SectionAction>(
-                        itemBuilder: (BuildContext context) => [
-                          const PopupMenuItem<SectionAction>(
-                              value: SectionAction.actionExportSection,
-                              child: Text('Export')),
-                          const PopupMenuItem<SectionAction>(
-                              value: SectionAction.actionEditSection,
-                              child: Text('Edit')),
-                          const PopupMenuItem<SectionAction>(
-                              value: SectionAction.actionDeleteSection,
-                              child: Text('Delete'))
-                        ],
-                        onSelected: (SectionAction sectionAction) async {
-                          switch (sectionAction) {
-                            case SectionAction.actionEditSection:
-                              await _sectionDialog(section);
-                            case SectionAction.actionDeleteSection:
-                              DbConnector.deleteSection(section);
-                              setState(() {
-                                sections =
-                                    DbConnector.getSections(widget._projectId);
-                              });
-                            case SectionAction.actionExportSection:
-                              await export(section);
-                          }
-                        },
-                      ),
-                      title: Text(section.name),
-                      isThreeLine: true,
-                      subtitle: Text(
-                          'Note: ${section.note}\nCreated: ${section.created.format()}'),
-                      onTap: () {
+                    return SectionCard(
+                      section: section,
+                      onScan: () {
                         Navigator.pushNamed(
                           context,
                           ScannerScreen.routeName,
@@ -135,15 +99,142 @@ class _SectionsScreenChildState extends State<_SectionsScreenChild> {
                           ),
                         );
                       },
+                      onDelete: () {
+                        DbConnector.deleteSection(section);
+                        setState(() {
+                          sections = DbConnector.getSections(widget._projectId);
+                        });
+                      },
+                      onEdit: () async {
+                        await _sectionDialog(section);
+                      },
+                      onExport: () async {
+                        await export(section);
+                      },
                     );
                   }),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () async => await _sectionDialog(null),
-        child: const Icon(Icons.add),
+        icon: const Icon(Icons.add),
+        label: const Text('New section'),
+      ),
+    );
+  }
+}
+
+class SectionCard extends StatelessWidget {
+  final Section section;
+  final void Function()? onScan;
+  final void Function()? onDelete;
+  final void Function()? onEdit;
+  final void Function()? onExport;
+
+  const SectionCard({
+    required this.section,
+    this.onScan,
+    this.onExport,
+    this.onDelete,
+    this.onEdit,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final exportSection = ExportSection(section);
+
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      elevation: 10,
+      color: Theme.of(context).colorScheme.secondaryContainer,
+      child: Stack(
+        fit: StackFit.loose,
+        alignment: AlignmentDirectional.topEnd,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  section.name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .displayLarge
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Description: ${section.note}',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        'Scanned items: ${exportSection.items.length}',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        exportSection.items.isEmpty
+                            ? ''
+                            : 'Latest update: ${exportSection.items[0].created.format()}',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton.icon(
+                        onPressed: () async => onScan?.call(),
+                        icon: const Icon(Icons.document_scanner_outlined),
+                        label: const Text('Scan')),
+                  ],
+                )
+              ],
+            ),
+          ),
+          ButtonBar(
+            children: [
+              IconButton(
+                  onPressed: () async => onExport?.call(),
+                  icon: const Icon(Icons.ios_share)),
+              PopupMenuButton<SectionAction>(
+                itemBuilder: (BuildContext context) => [
+                  const PopupMenuItem<SectionAction>(
+                      value: SectionAction.actionEditSection,
+                      child: Text('Edit')),
+                  const PopupMenuItem<SectionAction>(
+                      value: SectionAction.actionDeleteSection,
+                      child: Text('Delete'))
+                ],
+                onSelected: (SectionAction sectionAction) async {
+                  switch (sectionAction) {
+                    case SectionAction.actionEditSection:
+                      onEdit?.call();
+                    case SectionAction.actionDeleteSection:
+                      onDelete?.call();
+                    default:
+                  }
+                },
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
