5b721b1e5636bc7a1f368cbde63d63be48826d10
Adam Velebil <pardalek@gmail.com>
2024-10-13 21:44:30 +0200

    use dark theme on the scanner screen

--- lib/scanner/scan_duplicate.dart
+++ lib/scanner/scan_duplicate.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
+import 'package:stock_barcode_scanner/theme.dart';
 
 import '../domain/models.dart';
 
-class ScanDuplicate extends StatefulWidget {
+class ScanDuplicate extends ConsumerStatefulWidget {
   final ScannedItem scannedItem;
   final Function(ScannedItem) onUpdate;
   final Function() onClose;
@@ -15,10 +17,10 @@ class ScanDuplicate extends StatefulWidget {
       super.key});
 
   @override
-  State<ScanDuplicate> createState() => _ScanDuplicateState();
+  ConsumerState<ScanDuplicate> createState() => _ScanDuplicateState();
 }
 
-class _ScanDuplicateState extends State<ScanDuplicate> {
+class _ScanDuplicateState extends ConsumerState<ScanDuplicate> {
   int _currentValue = 0;
   int _originalValue = 0;
 
@@ -31,101 +33,105 @@ class _ScanDuplicateState extends State<ScanDuplicate> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomSheet(
-      onClosing: () {},
-      builder: (context) {
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 8),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: ColoredBox(
-                        color: Colors.transparent,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ListTile(
-                              title: const Text('Duplicate'),
-                              leading: Icon(
-                                Symbols.question_mark_rounded,
-                                color: Theme.of(context).colorScheme.primary,
+    return Theme(
+      data: ref.watch(themeDataProvider(Brightness.dark)),
+      child: BottomSheet(
+        onClosing: () {},
+        builder: (context) {
+          final textStyle = Theme.of(context).textTheme.bodyMedium;
+          final colorTheme = Theme.of(context).colorScheme;
+          final primaryColor = colorTheme.primary;
+          final baseTextStyle =
+              textStyle?.copyWith(color: colorTheme.onSurface);
+          final codeTextStyle =
+              TextStyle(color: primaryColor, fontWeight: FontWeight.bold);
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 8, 8, 8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: ColoredBox(
+                          color: Colors.transparent,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                title: const Text('Duplicate'),
+                                leading: Icon(
+                                  Symbols.question_mark_rounded,
+                                  color: primaryColor,
+                                ),
                               ),
-                            ),
-                            Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: RichText(
-                                    text:
-                                        TextSpan(text: 'The code ', children: [
-                                  TextSpan(
-                                      text: widget.scannedItem.barcode,
-                                      style: TextStyle(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                          fontWeight: FontWeight.bold)),
-                                  const TextSpan(
-                                      text: ' has been '
-                                          'recently scanned. To avoid duplicates, adjust '
-                                          'number of same codes manually.'),
-                                ]))),
-                            const Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Text('Or scan a new item.'),
-                            )
-                          ],
-                        )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 56, 8, 8),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        FilledButton.tonal(
-                          onPressed: () => _updateCount(1),
-                          child: const Icon(Symbols.add),
-                        ),
-                        Text(
-                          '$_currentValue',
-                          style: Theme.of(context).textTheme.headlineLarge,
-                        ),
-                        FilledButton.tonal(
-                          onPressed: () => _currentValue > _originalValue
-                              ? _updateCount(-1)
-                              : null,
-                          child: const Icon(Symbols.remove),
-                        ),
-                      ],
+                              Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: RichText(
+                                      text: TextSpan(
+                                          text: 'The code ',
+                                          style: baseTextStyle,
+                                          children: [
+                                        TextSpan(
+                                            text: widget.scannedItem.barcode,
+                                            style: codeTextStyle),
+                                        const TextSpan(
+                                            text: ' has been '
+                                                'recently scanned. To avoid duplicates, adjust '
+                                                'number of same codes manually.\n\nOr scan a new item.'),
+                                      ])))
+                            ],
+                          )),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _currentValue == _originalValue
-                      ? TextButton(
-                          onPressed: widget.onClose,
-                          child: const Text('Close'),
-                        )
-                      : TextButton.icon(
-                          onPressed: widget.onClose,
-                          label: const Text('Apply'),
-                          icon: const Icon(Symbols.check_rounded))
-                ],
-              )
-            ],
-          ),
-        );
-      },
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 56, 8, 8),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          FilledButton.tonal(
+                            onPressed: () => _updateCount(1),
+                            child: const Icon(Symbols.add),
+                          ),
+                          Text(
+                            '$_currentValue',
+                            style: Theme.of(context).textTheme.headlineLarge,
+                          ),
+                          FilledButton.tonal(
+                            onPressed: () => _currentValue > _originalValue
+                                ? _updateCount(-1)
+                                : null,
+                            child: const Icon(Symbols.remove),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _currentValue == _originalValue
+                        ? TextButton(
+                            onPressed: widget.onClose,
+                            child: const Text('Close'),
+                          )
+                        : TextButton.icon(
+                            onPressed: widget.onClose,
+                            label: const Text('Apply'),
+                            icon: const Icon(Symbols.check_rounded))
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 
--- lib/scanner/scan_success.dart
+++ lib/scanner/scan_success.dart
@@ -1,40 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:stock_barcode_scanner/theme.dart';
 
 import '../domain/models.dart';
 
-class ScanSuccess extends StatelessWidget {
+class ScanSuccess extends ConsumerWidget {
   final ScannedItem scannedItem;
 
   const ScanSuccess(this.scannedItem, {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return BottomSheet(
-      onClosing: () {},
-      builder: (context) {
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(8, 24, 8, 24),
-          child: Column(
-            children: [
-              ListTile(
-                  iconColor: Theme.of(context).colorScheme.primary,
-                  title: Text(scannedItem.barcode,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).colorScheme.primary)),
-                  subtitle: const Text('Scanned successfully'),
-                  leading: const Icon(
-                    Symbols.barcode,
-                    size: 48,
-                    //color: Colors.green,
-                  )),
-            ],
-          ),
-        );
-      },
-      // margin: const EdgeInsets.fromLTRB(8, 0, 8, 80),
-      // child: ,
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Theme(
+      data: ref.watch(themeDataProvider(Brightness.dark)),
+      child: BottomSheet(
+        onClosing: () {},
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(8, 24, 8, 24),
+            child: Column(
+              children: [
+                ListTile(
+                    iconColor: Theme.of(context).colorScheme.primary,
+                    title: Text(scannedItem.barcode,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary)),
+                    subtitle: const Text('Scanned successfully'),
+                    leading: const Icon(
+                      Symbols.barcode,
+                      size: 48,
+                    )),
+              ],
+            ),
+          );
+        },
+        // margin: const EdgeInsets.fromLTRB(8, 0, 8, 80),
+        // child: ,
+      ),
     );
   }
 }
